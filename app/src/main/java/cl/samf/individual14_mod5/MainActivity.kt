770aa57db9999e612f4ac4c197f87eb41f84971a
Sergio <sergiomolinaflores1@gmail.com>
2023-07-17 22:10:59 -0400

    Diseño y app Ok

--- app/src/main/java/cl/samf/individual14_mod5/MainActivity.kt
+++ app/src/main/java/cl/samf/individual14_mod5/MainActivity.kt
@@ -2,10 +2,52 @@ package cl.samf.individual14_mod5
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import cl.samf.individual14_mod5.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private var saldo = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonOk.setOnClickListener {
+
+            when (binding.radioGroup.checkedRadioButtonId) {
+                R.id.radioButtonSaldo ->
+                    binding.editTextMonto.setText(saldo.toString())
+
+                R.id.radioButtonIngresar ->
+                    ingresarSaldo()
+
+                R.id.radioButtonRetirar ->
+                    retirarSaldo()
+
+                R.id.radioButtonSalir -> finish()
+        }
+
+         }
+    }
+
+    private fun retirarSaldo() {
+        val monto = binding.editTextMonto.text.toString().toInt()
+        if (monto <= saldo) {
+            saldo -= monto
+            Toast.makeText(this, "Su saldo a sido actualizado", Toast.LENGTH_LONG).show()
+        } else {
+            Toast.makeText(this, "El monto a retirar supera el saldo", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun ingresarSaldo() {
+        saldo += binding.editTextMonto.text.toString().toInt()
+        Toast.makeText(this, "Su saldo a sido actualizado", Toast.LENGTH_LONG).show()
+
     }
 }
+
+
+
